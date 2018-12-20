af40e841cdc3430500e95662bf940136369a7cfa
devendran venkatesan <deva.venkat560@gmail.com>
2018-12-21 01:10:27 +0530

    set up production build and express-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
-module.exports = {
+module.exports = (env) => {
+  const isProduction = env === 'production';
+  return {
   entry: './src/app.js',
   output: {
     path: path.join(__dirname, 'public'),
@@ -20,8 +22,9 @@ module.exports = {
       ]
     }]
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: isProduction ? 'source-map':'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join(__dirname, 'public')
   }
+};
 };
